19b9efdf6c71e8fb3407ab376d442ae60bfbb33f
gabriel <gabrielfranksimonetto@gmail.com>
2019-10-19 20:24:36 -0300

    Ideas

---
+++ rigatonibot/cli.py
@@ -0,0 +1,19 @@
+# Ta esse eh o unico arquivo que soh eu tenho por enquanto:
+"""
+Ideias:
+1- fazer primeiro via cli, 
+    python rigatoni.py <arquivo>.py --> nao precisa ser py, mas acho que comece assim
+                                    --> a nao ser que converter pra txt sempre funcione
+        a) devolve o link criado no paste
+        b) abre direto no navegador o negocio criado
+
+2- depois dar um jeito de fazer isso virar uma ferramenta sh
+    rigatoni <arquivo>.py
+
+3- depois (ou antes) ja fazer um bot de telegrao
+    (eh bom pq dai a galera nao tem que instalar essa pomba no pc)
+
+4- oferecer outras ferramentas de paste
+    rigatoni <arquivo>  {paste, gist}
+                (ei, nao precisa ser .py!)
+"""
